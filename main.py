f64c2d551dd803179966be750746a1993ce7c1e2
jahidul2004 <islamjahiduljihad@gmail.com>
2025-05-07 19:17:57 +0600

    Car positions dynamic added.

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ track = pygame.image.load("assets/track1.png")
 car = pygame.image.load("assets/car.png")
 car = pygame.transform.scale(car,(40,80))
 
+#Car positions
+car_x = 146
+car_y = 260
+
 #Condition
 drive = True;
 while drive:
@@ -19,5 +23,5 @@ while drive:
         if event.type == pygame.QUIT:
             drive = False;
     window.blit(track, (0,0))
-    window.blit(car,(146,260))
+    window.blit(car,(car_x,car_y))
     pygame.display.update()
