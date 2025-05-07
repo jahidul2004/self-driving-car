f61695a02e484f3bb6330cf7a0b217509dd4ee32
jahidul2004 <islamjahiduljihad@gmail.com>
2025-05-07 19:01:12 +0600

    Window created and track1 loaded.

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import pygame
 pygame.init()
+
+# Setup the window
+window = pygame.display.set_mode((1200,400))
+
+
+#Load the track
+track = pygame.image.load("assets/track1.png")
+
+#Condition
+while True:
+    window.blit(track, (0,0))
+    pygame.display.update()
