68bcd92b82fed2ffa22384ccd9e6c06a139e5985
jahidul2004 <islamjahiduljihad@gmail.com>
2025-05-07 19:07:08 +0600

    Window closing system added.

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ window = pygame.display.set_mode((1200,400))
 track = pygame.image.load("assets/track1.png")
 
 #Condition
-while True:
+drive = True;
+while drive:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            drive = False;
     window.blit(track, (0,0))
     pygame.display.update()
